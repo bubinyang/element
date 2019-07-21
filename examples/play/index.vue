b9706a70c2ef54514192764111456fbddfd6e3ba
bubinyang <bubinyang@163.com>
2019-07-21 20:05:34 +0800

    测试一下

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div style="margin: 20px;">
-<<<<<<< HEAD
-    <el-input v-model="input" placeholder="请输入内容"></el-input>
-=======
     <el-input v-model="input" placeholder="请输入内容1"></el-input>
->>>>>>> 测是
   </div>
 </template>
 
