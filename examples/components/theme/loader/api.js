6f1a3a748e438c6ca6fdbd18892f91929ee45909
bubinyang <bubinyang@163.com>
2019-07-21 22:04:31 +0800

    呵呵

---
+++ examples/components/theme/loader/api.js
@@ -0,0 +1,28 @@
+import Element from 'main/index.js';
+import { post, get } from './ajax';
+
+const { version } = Element;
+
+const hostList = {
+  local: 'http://localhost:3008/',
+  alpha: 'https://element-api.ar.elenet.me/element/theme/',
+  production: 'https://element-api.ele.me/element/theme/'
+};
+
+const host = hostList[process.env.FAAS_ENV] || hostList.production;
+
+export const getVars = () => {
+  return get(`${host}getVariable?version=${version}`);
+};
+
+export const getTestEle = () => {
+  return get(`${hostList.alpha}getVariable`);
+};
+
+export const getTestAli = () => {
+  return get(`https://private-alipayobjects.alipay.com/alipay-rmsdeploy-image/rmsportal/VmvVUItLdPNqKlNGuRHi.png?t=${Date.now()}`);
+};
+
+export const updateVars = (data, cb) => {
+  return post(`${host}updateVariable?version=${version}`, data, cb);
+};
